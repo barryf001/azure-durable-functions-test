de1652f2d3384a31c49f701a727fafa9e5379aae
Barry Fitzgerald <barry.fitzgerald@aqovia.com>
2021-10-07 15:17:13 +0100

    bump:minor

--- src/TestLib/Class1.cs
+++ src/TestLib/Class1.cs
@@ -5,6 +5,7 @@ namespace TestLib
     public class Class1
     {
         const string IMPROVE_CONCURRENT_LOGGING="IMPROVE_CONCURRENT_LOGGING";
+        const string NEW_FUNCTIONALITY="NEW_FUNCTIONALITY";
 
     }
 }
