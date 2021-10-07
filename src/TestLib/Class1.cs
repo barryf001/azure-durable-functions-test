172ae8bc8f28bad0ad4eb53aae35c6a7f4884b7f
Barry Fitzgerald <barry.fitzgerald@aqovia.com>
2021-10-07 15:55:09 +0100

    bump:patch

--- src/TestLib/Class1.cs
+++ src/TestLib/Class1.cs
@@ -6,6 +6,7 @@ namespace TestLib
     {
         const string IMPROVE_CONCURRENT_LOGGING="IMPROVE_CONCURRENT_LOGGING";
         const string NEW_FUNCTIONALITY="NEW_FUNCTIONALITY";
+        const string NEW_FUNCTIONALITY_2="NEW_FUNCTIONALITY_2";
 
     }
 }
